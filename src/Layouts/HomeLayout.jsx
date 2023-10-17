c32a3de9539eb5fa763389b952651b8abe88fc78
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-17 13:43:22 +0530

    about page made

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -55,7 +55,7 @@ function HomeLayout({ children }) {
             <div className="drawer-side w-0 h-screen">
                 <label htmlFor="my-drawer" className='drawer-overlay '>
                 </label>
-                <ul className="menu p-4 w-48 sm:w-80 bg-base-300 text-base-content relative">
+                <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-300 text-base-content relative">
                     <li className="w-fit absolute right-2 z-50">
                         <button onClick={hideDrawer}>
                             <AiFillCloseCircle size={24} />
@@ -82,7 +82,7 @@ function HomeLayout({ children }) {
                     </li>
                     {
                         !isLoggedIn &&(
-                            <div className="w-full flex items-center justify-center flex-col mt-6 gap-y-4">
+                            <div className="w-full absolute bottom-0 flex items-center justify-center flex-col mt-6 gap-y-4">
                                 <button className='btn-primary px-3 py-2 font-semibold rounded-md w-full'>
                                     <Link to='/login'>Login</Link>
                                 </button>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,78 @@
+import HomeLayout from "../Layouts/HomeLayout";
+
+import aboutMainImage from "../assets/aboutMainImage.png";
+import apj from '../assets/apj.png'
+import billGates from '../assets/billGates.png'
+import einstein from '../assets/einstein.png'
+import nelsonMandela from '../assets/nelsonMandela.png'
+import steveJobs from '../assets/steveJobs.png'
+
 
 function AboutUs() {
   return (
-    <div>
-        
-    </div>
-  )
+    <HomeLayout>
+      <div className="pl-20 pt-20 flex flex-col text-white">
+        <div className="flex items-center gap-5 mx-10 ">
+          <section className="w-1/2 space-y-10">
+            <h1 className="text-5xl text-yellow-500 font-semibold">
+              Affordable and quality education
+            </h1>
+            <p className="text-xl text-gray-200 ">
+              Our goal is to provide the affordable and quality education to the
+              world. We are providing the platform for the aspiring teachers and
+              students to share their skills.
+            </p>
+          </section>
+          <div className="w-1/2">
+            <img
+              id="test1"
+              src={aboutMainImage}
+              alt=""
+              className="drop-shadow-2xl"
+            />
+          </div>
+        </div>
+
+        <div className="carousel w-1/2 my-16 m-auto h-auto">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img src={apj} className="w-full border rounded-full p-4" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide5" className="btn btn-circle"> ❮ </a>
+              <a href="#slide2" className="btn btn-circle"> ❯ </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img src={billGates} className="w-full border rounded-full p-4" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle"> ❮ </a>
+              <a href="#slide3" className="btn btn-circle"> ❯ </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img src={steveJobs} className="w-full border rounded-full p-4" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle"> ❮ </a>
+              <a href="#slide4" className="btn btn-circle"> ❯ </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img src={nelsonMandela} className="w-full border rounded-full p-4" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle"> ❮ </a>
+              <a href="#slide5" className="btn btn-circle"> ❯ </a>
+            </div>
+          </div>
+          <div id="slide5" className="carousel-item relative w-full">
+            <img src={einstein} className="w-full border rounded-full p-4" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle"> ❮ </a>
+              <a href="#slide1" className="btn btn-circle"> ❯ </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HomeLayout>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
